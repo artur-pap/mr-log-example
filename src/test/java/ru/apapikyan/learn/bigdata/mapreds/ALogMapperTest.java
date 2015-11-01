b6a7929e57e7390cdb602ab66ed0890d2a2f2969
art-papikyan <art-papikyan@yandex.ru>
2015-11-01 13:36:51 +0300

    Changes to be committed: 	deleted:    eclipse.gitignore 	modified:   pom.xml 	deleted:    src/main/resources/log4j.properties 	modified:   src/test/java/ru/apapikyan/learn/bigdata/mapreds/ALogMapperTest.java 	modified:   src/test/java/ru/apapikyan/learn/bigdata/mapreds/ALogReducerTest.java

--- src/test/java/ru/apapikyan/learn/bigdata/mapreds/ALogMapperTest.java
+++ src/test/java/ru/apapikyan/learn/bigdata/mapreds/ALogMapperTest.java
@@ -1,19 +1,12 @@
 package ru.apapikyan.learn.bigdata.mapreds;
 
-import static org.junit.Assert.assertThat;
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Counter;
-import org.apache.hadoop.mapreduce.Counters;
 import org.apache.hadoop.mrunit.mapreduce.MapDriver;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.Is;
 import org.junit.Test;
-
-import com.google.inject.matcher.Matchers;
 
 public class ALogMapperTest {
 
@@ -24,7 +17,6 @@ public class ALogMapperTest {
 	 */
 	@Test
 	public void processesValidRecord() throws IOException, InterruptedException {
-		String inpString = "ip2 - - ";
 		Text value = new Text(
 		        "ip2 - - " +
 		        //IP
@@ -34,8 +26,7 @@ public class ALogMapperTest {
 		        //Bytes
 		        "\"http://www.stumbleupon.com/refer.php?url=http%3A%2F%host1%2Fsun_ss5%2F\" \"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16\"");
 				//ignore
-		
-		
+
 		new MapDriver<LongWritable, Text, Text, IntWritable>()
 			.withMapper(new ALogMapper())
 			.withInput(new LongWritable(0), value)
@@ -48,22 +39,17 @@ public class ALogMapperTest {
 		Text value = new Text(
                 "ip2" +
 		        //IP
-                " - - [24/Apr/2011:04:20:11 -0400] \"GET /sun_ss5/ HTTP/1.1\" 200 " +
+                " - -\"\"\"\"\" [24/Apr/2011:04:20:11 -0400] \"GET /sun_ss5/ HTTP/1.1\" 200 " +
                 //ignore
-                "1sdf49sdf17" +
+                "\"1sdf49s\"\"df17" +
                 //Bytes
-                " \"http://www.stumbleupon.com/refer.php?url=http%3A%2F%host1%2Fsun_ss5%2F\" \"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16\"");
+                " \"h\"ttp://www\".\"stumbleupon\".\"com\"/\"refer\".php?url=http%3A%2F%host1%2Fsun_ss5%2F\" \"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.16) Gecko/20110319 Firefox/3.6.16\"");
                 //ignore rest
-
-		Counters counters = new Counters();
 
 		new MapDriver<LongWritable, Text, Text, IntWritable>()
 			.withMapper(new ALogMapper())
 		    .withInput(new LongWritable(0), value)
 		    .runTest();
-		
-		Counter c = counters.findCounter(ALogMapper.LogRecord.BAD);
-		assert (new Long("1").equals(c.getValue()));
 	}
 	
 	@Test
@@ -73,24 +59,18 @@ public class ALogMapperTest {
 		        //IP
                 " - - [24/Apr/2011:09:01:35 -0400] \"GET /sunFAQ/serial/ HTTP/1.0\" 200 " +
                 //ignore
-                "13600" +
+                "0" +
                 //Bytes
                 " \"-\" \"Mozilla/5.0 (compatible; MJ12bot/v1.3.3; http://www.majestic12.co.uk/bot.php?+)\"");
                 //ignore rest
 
-		Counters counters = new Counters();
-
 		new MapDriver<LongWritable, Text, Text, IntWritable>()
 			.withMapper(new ALogMapper())
 		    .withInput(new LongWritable(0), value)
+		    .withOutput(new Text("ip86"), new IntWritable(0))
 		    .runTest();
-		
-		Counter c = counters.findCounter(ALogMapper.LogRecord.BAD);
-		assert (new Long("1").equals(c.getValue()));
 	}
-	/**
-	 * 
+	/*
 	 * @formatter:on
-	 *
 	 */
 }
--- src/test/java/ru/apapikyan/learn/bigdata/mapreds/ALogReducerTest.java
+++ src/test/java/ru/apapikyan/learn/bigdata/mapreds/ALogReducerTest.java
@@ -3,30 +3,34 @@ package ru.apapikyan.learn.bigdata.mapreds;
 import java.io.IOException;
 import java.util.Arrays;
 
-import org.apache.hadoop.io.FloatWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mrunit.ReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
 import org.junit.Test;
 
 public class ALogReducerTest {
 
-//	@Test
-//	public void returnsAvgBytesPerIP() throws IOException, InterruptedException {
-//		ALogReducer  somreducer = null;
-//		new ReduceDriver<Text, IntWritable, Text, IntWritable>()
-//		        .withReducer(new ALogReducer())
-//		        .withInput(new Text("IP7678"),
-//		                Arrays.asList(new IntWritable(234), new IntWritable(12456), new IntWritable(236789)))
-//		        .withOutput(new Text("IP7678"), new FloatWritable(249245)).runTest();
-//	}
+	/*
+	 * @formatter:off
+	 */
+	@Test
+	public void returnsAvgBytesPerIP() throws IOException, InterruptedException {
 
-//	@Test
-//	public void returnsTotalBytesPerIP() throws IOException, InterruptedException {
-//		new ReduceDriver<Text, IntWritable, Text, IntWritable>()
-//        .withReducer(new ALogReducer())
-//        .withInput(new Text("IP7678"),
-//                Arrays.asList(new IntWritable(234), new IntWritable(12456), new IntWritable(236789)))
-//        .withOutput(new Text("IP7678"), new FloatWritable(249245)).runTest();
-//	}
+		new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+		        .withReducer(new ALogReducer())
+		        .withInput(new Text("IP7678"), Arrays.asList(new IntWritable(234), new IntWritable(12456), new IntWritable(236789)))
+		        .withOutput(new Text("IP7678"), new IntWritable(249479)).runTest();
+	}
+
+	@Test
+	public void returnsTotalBytesPerIP() throws IOException, InterruptedException {
+
+		new ReduceDriver<Text, IntWritable, Text, IntWritable>()
+		        .withReducer(new ALogReducer())
+		        .withInput(new Text("IP7678"), Arrays.asList(new IntWritable(234), new IntWritable(12456), new IntWritable(236789)))
+		        .withOutput(new Text("IP7678"), new IntWritable(249479)).runTest();
+	}
+	/*
+	 * @formatter:on
+	 */
 }
